e4b6826f1269a341e059c9912a8e0a496d1d50bb
vishwas3332 <vishwas3332@gmail.com>
2023-03-13 14:30:38 +0530

    added news page

--- prospert/feed/views.py
+++ prospert/feed/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from newsapi import NewsApiClient
 # Create your views here.
 
 def feedPage(request):
-	return render(request, 'feed/feedPage.html')
+	feedApi = NewsApiClient(api_key='37c69975025a4cec803e6aafbb03fe5a')
+	headlines = feedApi.get_top_headlines(category='business',country='in')
+	articles = headlines['articles']
+	context = {'articles':articles}
+	return render(request, 'feed/feedPage.html',context)
